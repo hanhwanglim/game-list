f0ed8d0b3e19442d26ba65d8d8f0ef66ec426157
sc19hhl <sc19hhl@leeds.ac.uk>
2020-12-10 01:44:31 +0800

    created login functionality

--- app/views.py
+++ app/views.py
@@ -12,6 +12,11 @@ def index():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """
+    Redirects to signup.html if the request method is a GET method.
+    Otherwise, if the request method is a POST method, then it will 
+    create a new user with the user details and add it into the database.
+    """
     form = RegisterForm()
     if form.validate_on_submit():
         # Find if username or email is in database
@@ -31,7 +36,7 @@ def signup():
         elif find_user_username is not None:
             flash('Username has already been taken.')
             return render_template('signup.html', form=form)
-            
+
         # Adding user into the user database
         user = User(email=new_email, username=new_username, password=generate_password_hash(new_password, method='sha256'))
         db.session.add(user)
@@ -42,8 +47,19 @@ def signup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Redirects to login.html if the request method is a GET method.
+    Otherwise, if the request method is a POST method, then it will 
+    query and verify the user and return to their feed.
+    """
     form = LoginForm()
     if form.validate_on_submit():
-        print("hello")
-        return "hello"
+        username = request.form.get("username")
+        password = request.form.get("password")
+        remember = True if request.form.get('remember') else False
+        user = User.query.filter_by(username=username).first()
+        if user is None or not check_password_hash(user.password, password):
+            flash("Username or password incorrect.")
+            return render_template("login.html", form=form)
+        return "success"
     return render_template('login.html', form=form)
