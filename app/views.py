d397a3662bf64dca2e4ac41a9792bba1b0a8bf6d
sc19hhl <sc19hhl@leeds.ac.uk>
2020-12-10 01:25:24 +0800

    updated signup to store new users into database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, request
-from app import app
+from flask import render_template, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import app, db
 from app.forms import RegisterForm, LoginForm
+from app.models import User
 
 
 @app.route('/', methods=['GET'])
@@ -12,8 +14,29 @@ def index():
 def signup():
     form = RegisterForm()
     if form.validate_on_submit():
-        print("hello")
-        return "hello"
+        # Find if username or email is in database
+        new_email = request.form.get("email")
+        new_username = request.form.get("username")
+        new_password = request.form.get("password")
+        find_user_email = User.query.filter(User.email==new_email).first()
+        find_user_username = User.query.filter(User.username==new_username).first()
+
+        # If a user with the same email or username is found then return error
+        if find_user_email and find_user_username is not None:
+            flash('Username and email has already exist.')
+            return render_template('signup.html', form=form)
+        elif find_user_email is not None:
+            flash('Email has already exist.')
+            return render_template('signup.html', form=form)
+        elif find_user_username is not None:
+            flash('Username has already been taken.')
+            return render_template('signup.html', form=form)
+            
+        # Adding user into the user database
+        user = User(email=new_email, username=new_username, password=generate_password_hash(new_password, method='sha256'))
+        db.session.add(user)
+        db.session.commit()
+        return "success"
     return render_template('signup.html', form=form)
 
 
